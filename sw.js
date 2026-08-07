258045e742ae32913f600d8cd0d5c97624a3dee9
SKDC_SMART <somchaibutphon@gmail.com>
2026-08-07 21:30:27 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,18 @@
-const CACHE="wvf-static-queue-standard-20260807-r37";
-const SHELL=["./","./index.html","./styles.css","./config.js","./app.js","./sweetalert2.all.min.js","./jsqr.js","./manifest.webmanifest","./favicon.png","./icon-192.png","./icon-512.png","./apple-touch-icon.png","./queue.html","./queue.css","./queue.js"];
+const CACHE="wvf-static-queue-force-refresh-20260807-r41";
+const SHELL=["./","./index.html","./styles.css","./config.js","./app.js","./sweetalert2.all.min.js","./jsqr.js","./manifest.webmanifest","./favicon.png","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
   if(event.request.method!=="GET")return;
   const url=new URL(event.request.url);if(url.origin!==self.location.origin)return;
+  const isQueue=/\/(queue\.html|queue\.css|queue\.js)$/.test(url.pathname);
+  if(isQueue){
+    event.respondWith(fetch(event.request,{cache:"no-store"}).then(response=>{
+      if(response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));}
+      return response;
+    }).catch(()=>caches.match(event.request)));
+    return;
+  }
   event.respondWith(fetch(event.request).then(response=>{
     if(response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));}
     return response;
