7719346b7da0025f2e624cdb445611380374afa5
SKDC_SMART <somchaibutphon@gmail.com>
2026-08-17 15:12:15 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX="wvf-static-";
-const CACHE="wvf-static-round162-production-hardening";
+const CACHE="wvf-static-round163-performance-resilience";
 const CORE=["./","./index.html","./styles.css","./config.js","./app.js","./sweetalert2.all.min.js","./jsqr.js","./manifest.webmanifest"];
 const OPTIONAL=["./favicon.png","./icon-192.png","./icon-512.png","./apple-touch-icon.png","./track.html","./track.css","./track.js","./queue.html","./queue.css","./queue.js","./voice-engine.js"];
 
